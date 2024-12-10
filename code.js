d95b43202b528637c41cf229527cfeaa8ad82731
chenry900 <158115154+chenry900@users.noreply.github.com>
2024-12-10 12:02:37 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,24 @@
 import java.util.PriorityQueue;
 
+class DijkstraNode {
+    var distance;
+    var currentNode;
+    
+    constructor (node, distance) {
+        this.currentnode = node;
+        this.distance = distance;
+    }
+    Node() {
+        return currentNode;
+    }
+    distance() {
+        return distance;
+    }
+    newDistance(distance) {
+        this.distance = distance;
+    }
+
+}
 
 are_isomorphic(graph1, graph2) {
     var DiGraph1 = dijkstra(graph1, 0);
@@ -17,33 +36,32 @@ are_isomorphic(graph1, graph2) {
 //This assumes that dijkstra's works and compairs the 2 graph distances to check if they are isomorphic
 
 function dijkstra(graph, sourceNode) {
-    var DijMatrix = [];
-    boolean visited = [];
-    var check, dist;
+    var[] Dijkstra = new var[graph.length];
+    boolean[] visited = new boolean[graph.length];
     var inf = 999;
-    PriorityQueue<integer> priority = new PriorityQueue<>();
+    PriorityQueue<DijkstraNode> priority = new PriorityQueue<>(node, node.distance);
 
     for (var i = 0; i < graph.length; i++) {
-        if (i == 0){
-            DijMatrix[i] = 0;
-            visited.push(true);
+        if (i == sourceNode){
+            Dijkstra[i] = new DijkstraNode(sourceNode, inf);
+            visited[i] = true;
         }
         else {
-            DijMatrix[i] = inf;
-            visited.push(false);
+            Dijkstra[i] = new DijkstraNode(i, inf);
+            visited[i] = false;
         }
     }
-    priority.add(0, 0);
+    priority.add(Dijkstra[sourceNode]);
 
     while (!priority.isEmpty()) {
-        check = priority.poll();
+        DijkstraNode check = priority.poll();
         for (var i = 0; i < graph.length; i++) {
-            if (graph[check][i] > 0) {
-                dist = DijMatrix[check] + graph[check][i];
-                if (dist < DijMatrix[i]) {
-                    DijMatrix[i] = dist;
+            if (graph[check.Node()][i] > 0) {
+                var newdist = check.distance() + graph[check.Node()][i];
+                if (newdist < Dijkstra[check.Node()].distnace()) {
+                    Dijkstra[check.Node()].newDistance(newdist);
                     if (!visited[i]) {
-                        priority.add(i, DijMatrix[i]);
+                        priority.add(Dijkstra[i]);
                         visited[i] = true;
                     }
                 }
